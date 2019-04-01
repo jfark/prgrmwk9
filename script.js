0e9edf89c17fc01877e2eca8e2604f3fbacf2e76
jfark <jessamyn.farkas@gmail.com>
2019-03-31 22:44:11 -0400

    Trying to Fix

--- script.js
+++ script.js
@@ -3,6 +3,15 @@
 var llamaFactoids = new Vue({
 	el: '.factoids',
 	data: {
-		factoid1: "test"
+		factoid1: "test",
+		factoid2: "test",
+		factoid3: "test",
+		factoid4: "test",
+		factoid5: "test",
+		factoid6: "test",
+		factoid7: "test",
+		factoid8: "test",
+		factoid9: "test",
+		factoid10: "test",
 	}
 })
