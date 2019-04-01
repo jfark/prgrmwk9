7948cd4526c5f2f4fa72f1c7dfa6ce4323692498
jfark <jessamyn.farkas@gmail.com>
2019-03-31 23:24:22 -0400

    Latest

--- script.js
+++ script.js
@@ -3,15 +3,15 @@
 var llamaFactoids = new Vue({
 	el: '.factoids',
 	data: {
-		factoid1: "test",
-		factoid2: "test",
-		factoid3: "test",
-		factoid4: "test",
-		factoid5: "test",
-		factoid6: "test",
-		factoid7: "test",
-		factoid8: "test",
-		factoid9: "test",
-		factoid10: "test",
+		factoid1: "Llamas can reach speeds up to 35mph!",
+		factoid2: "Llamas are native to the South American Andes.",
+		factoid3: "Llamas can carry 50 to 75 pounds up to 20 miles.",
+		factoid4: "Llamas spit for many different reasons - spitting is essentially just part of their vocabulary :P",
+		factoid5: "Llamas are vegetarians.",
+		factoid6: "Llamas carried the materials used in the building of the ancient city of Machu Picchu!",
+		factoid7: "Llamas' eyes are designed with special sunshades built in, that look 'like a curtain of ruffles hanging in the eyeball, and close like Venetian blinds when needed.' (!?!?)",
+		factoid8: "Llamas have specially shaped blood cells to store oxygen in high altitudes.",
+		factoid9: "Llamas in herds hum in unison.",
+		factoid10: "Llamas' dung, once dried, can be burned for fuel!",
 	}
 })
