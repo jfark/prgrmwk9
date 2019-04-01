5d5779dba63731e1cb235aff5ec616d3c9127ca3
jfark <jessamyn.farkas@gmail.com>
2019-04-01 00:11:12 -0400

    Having Trouble with My Conditional Statement

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 //Underrated Llamas
 
+var spittingOffense = new Vue({
+  el: '.spitter',
+  data: {
+    rudeSpitting: window.confirm("Does spitting offend you? Press Yes if so, and press Cancel otherwise.")
+  }
+})
+
 var llamaFactoids = new Vue({
 	el: '.factoids',
 	data: {
 		factoid1: "Llamas can reach speeds up to 35mph!",
 		factoid2: "Llamas are native to the South American Andes.",
 		factoid3: "Llamas can carry 50 to 75 pounds up to 20 miles.",
-		factoid4: "Llamas spit for many different reasons - spitting is essentially just part of their vocabulary :P",
+		factoid4: "Llamas' dung, once dried, can be burned for fuel!",
 		factoid5: "Llamas are vegetarians.",
 		factoid6: "Llamas carried the materials used in the building of the ancient city of Machu Picchu!",
-		factoid7: "Llamas' eyes are designed with special sunshades built in, that look 'like a curtain of ruffles hanging in the eyeball, and close like Venetian blinds when needed.' (!?!?)",
+		factoid7: "Llamas' eyes are designed with special sunshades built in, that look like a curtain of ruffles hanging in the eyeball, and close like Venetian blinds when needed. (!?!?)",
 		factoid8: "Llamas have specially shaped blood cells to store oxygen in high altitudes.",
 		factoid9: "Llamas in herds hum in unison.",
-		factoid10: "Llamas' dung, once dried, can be burned for fuel!",
+		factoid10: "Llamas spit for many different reasons - spitting is essentially just part of their vocabulary :P",
 	}
 })
